5392061d0f631f8deec979f1f45eabb80b3eb523
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-01-25 20:21:36 -0500

    Set up a bare bones shiny app

---
+++ app.R
@@ -0,0 +1,20 @@
+library(shiny)
+
+drumkitUI <- function(id) {
+  ns <- NS(id)
+  tagList(
+
+  )
+}
+
+drumkit <- function(input, output, session, ...) {
+}
+
+ui <- fluidPage(
+)
+
+server <- function(input, output, session) {
+
+}
+
+shinyApp(ui, server)
